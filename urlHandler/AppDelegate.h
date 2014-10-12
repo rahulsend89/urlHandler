36065e55882e8533b7c06a59f74e2ec5296a0e92
Rahul Malik <rahul.send89@gmail.com>
2014-10-12 15:30:45 +0530

    Initial Commit

---
+++ urlHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  urlHandler
+//
+//  Created by Rahul Malik on 12/10/14.
+//  Copyright (c) 2014 Rahul Malik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ urlHandler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  urlHandler
+//
+//  Created by Rahul Malik on 12/10/14.
+//  Copyright (c) 2014 Rahul Malik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
